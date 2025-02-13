e3ae8ae24bb0214f469779282b3c6f305e3629a6
obydul960 <obydulhaque960@gmail.com>
2025-02-13 11:36:11 +0600

    age and gender detact

--- src/Components/Camera.js
+++ src/Components/Camera.js
@@ -1,13 +1,17 @@
+import * as faceapi from 'face-api.js';
 import React, { Component, Fragment, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import imagePlaceholder from "../assets/images/261694-200.png";
 import Webcam from "react-webcam";
 import SweetAlert from "react-bootstrap-sweetalert";
+import ReactJson from "react-json-view";
 
 const Camera = () => {
   const cameraRef = React.createRef();
   const [capturedPhoto, setCapturedPhoto] = useState(imagePlaceholder);
   const [cameraError, setCameraError] = useState(false);
+  const [ageAndGender, setAgeAndGender] = useState([]);
+  console.log('ageAndGender ===',ageAndGender);
   // just get captchure picture
   const onCapture = async() => {
     setCapturedPhoto(cameraRef.current.getScreenshot());
@@ -42,12 +46,24 @@ const Camera = () => {
     window.location.href = "/";
   };
 
+  //age and gender define
+  async function ageAndGenderDetection(){
+      await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+      await faceapi.nets.ageGenderNet.loadFromUri('/models');
+      var images = document.getElementById('first-img');
+      const ageAndGender  = await faceapi.detectAllFaces(images).withAgeAndGender();
+      setAgeAndGender(ageAndGender);
+  
+  }
+
+  const damiJson = [{name:"Demo one",Age:25}];
+
   return (
     <div>
       <Fragment>
         <Container>
           <Row className="mt-5 shadow-sm bg-white">
-            <Col className="p-2" md={6} sm={12} lg={6}>
+            <Col className="p-2 text-center" md={4} sm={12} lg={4}>
               <Webcam
                 onUserMediaError={onCameraError}
                 ref={cameraRef}
@@ -62,11 +78,40 @@ const Camera = () => {
                 Capure
               </button>
             </Col>
-            <Col className="p-2" md={6} sm={12} lg={6}>
-              <img className="w-100" src={capturedPhoto} />
+            <Col className="p-2 text-center" md={4} sm={12} lg={4}>
+              <img id="first-img" className="w-100 img" src={capturedPhoto} />
               <button onClick={onSave} className="btn mt-3 btn-lg btn-primary">
                 Save
               </button>
+            </Col>
+            <Col className="p-2 text-center" md={4} sm={12} lg={4}>
+            <p>Age :</p>
+            <p>Gender :</p>
+            <p>Expression :</p>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container>
+          <Row>
+            <Col className="p-2" sm={12} md={6} lg={6}>
+            <h2>Face Landmrk</h2> 
+            <button onClick={ageAndGenderDetection} className="btn mt-3 btn-lg btn-primary">
+                Get Age & Gender
+              </button>
+            <ReactJson src={damiJson} theme="monokai" />
+            </Col>
+            <Col className="p-2" sm={12} md={6} lg={6}>
+            <h2>Face Expression</h2> 
+            <ReactJson src={damiJson} theme="monokai" />
+            </Col>
+            <Col className="p-2" sm={12} md={6} lg={6}>
+            <h2>Age Estami</h2> 
+            <ReactJson src={damiJson} theme="monokai" />
+            </Col>
+            <Col className="p-2" sm={12} md={6} lg={6}>
+            <h2>Gender Recognition</h2> 
+            <ReactJson src={damiJson} theme="monokai" />
             </Col>
           </Row>
         </Container>
